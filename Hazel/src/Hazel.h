2f92f68dbf5fedefed4f9d5a8f04c09a9b9f7fbe
洪硕 张 <zhszhs1100@outlook.com>
2024-03-12 22:08:46 +0800

    Add project files.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// For use by Hazel applications
+
+#include "Hazel/Application.h"
+#include "Hazel/Layer.h"
+#include "Hazel/Log.h"
+
+#include "Hazel/ImGui/ImGuiLayer.h"
+
+// ---Entry Point---------------------
+#include "Hazel/EntryPoint.h"
+// -----------------------------------
