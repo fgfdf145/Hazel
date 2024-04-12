26884fe3d5b42ea5bf0552b6b2a4085dd532e8f1
fgfdf14 <zhszhs1100@gmail.com>
2024-04-12 15:13:27 +0800

    Encapsulation VertexArray api, and modify Application cpp

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -1,14 +1,14 @@
 #pragma once
-
 #include "Core.h"
-
 #include "Window.h"
 #include "Hazel/LayerStack.h"
 #include "Hazel/Events/Event.h"
 #include "Hazel/Events/ApplicationEvent.h"
-#include "Hazel/Renderer/Buffer.h"
 #include "Hazel/ImGui/ImGuiLayer.h"
+
 #include "Hazel/Renderer/Shader.h"
+#include "Hazel/Renderer/Buffer.h"
+#include "Hazel/Renderer/VertexArray.h"
 
 namespace Hazel 
 {
@@ -18,35 +18,27 @@ namespace Hazel
 	public:
 		Application();
 		virtual ~Application();
-
 		void Run();
-
 		void OnEvent(Event& e);
-
 		void PushLayer(Layer* layer);
 		void PushOverlay(Layer* layer);
-
 		inline Window& GetWindow() { return *m_Window; }
-
 		inline static Application& Get() { return *s_Instance; }
 	private:
 		bool OnWindowClose(WindowCloseEvent& e);
-
 		std::unique_ptr<Window> m_Window;
 		ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray;
+		std::shared_ptr<Shader> m_Shader;
+		std::shared_ptr<VertexArray> m_VertexArray;
 
-		std::unique_ptr<Shader> m_Shader;
-		std::unique_ptr<VertexBuffer> m_VertexBuffer;
-		std::unique_ptr<IndexBuffer> m_IndexBuffer;
+		std::shared_ptr<Shader> m_BlueShader;
+		std::shared_ptr<VertexArray> m_SquareVA;
 	private:
 		static Application* s_Instance;
 	};
-
 	// To be defined in CLIENT
 	Application* CreateApplication();
-
 }
