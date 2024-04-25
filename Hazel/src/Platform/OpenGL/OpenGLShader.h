68608c78e79458747f6c1e70271861ad7d705497
fgfdf14 <zhszhs1100@gmail.com>
2024-04-25 13:33:49 +0800

    Add shader library

--- Hazel/src/Platform/OpenGL/OpenGLShader.h
+++ Hazel/src/Platform/OpenGL/OpenGLShader.h
@@ -11,12 +11,14 @@ namespace Hazel
 	{
 	public:
 		OpenGLShader(const std::string& filepath);
-		OpenGLShader(const std::string& vectexSrc, const std::string& fragmentSrc);
+		OpenGLShader(const std::string& name, const std::string& vertexSrc, const std::string& fragmentSrc);
 		
 		virtual ~OpenGLShader();
 
 		virtual void Bind() const override;
 		virtual void Unbind() const override;
+
+		virtual const std::string& GetName() const override { return m_Name; }
 
 		void UploadUniformInt(const std::string& name, int value);
 
@@ -33,5 +35,6 @@ namespace Hazel
 		void Compile(const std::unordered_map<GLenum, std::string>& shaderSource);
 
 		uint32_t m_RendererID;
+		std::string m_Name;
 	};
 }
